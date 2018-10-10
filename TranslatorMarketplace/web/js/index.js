84a652e31f2d5aed0286019f3acdb594ab9ae614
Anatda Planoi <59070184@it.kmitl.ac.th>
2018-10-10 14:40:33 +0700

    เพิ่ม create order
    เพิ่ม css js create_oder.html

---
+++ TranslatorMarketplace/web/js/index.js
@@ -0,0 +1,56 @@
+
+//uplode file//
+$(function() {
+  function init() {
+		$('[data-behaviour="custom-upload-input"]').on('change', updateButton);
+  };
+  
+  function updateButton(e) {
+		var inputValue = $(e.currentTarget).val().split( '\\' ).pop()
+		$('[data-element="custom-upload-button"]').text(inputValue)
+		e.preventDefault;
+		console.log(inputValue)
+  };
+  
+  init()
+});
+//calculate price//
+/* Set rates + misc */
+var fadeTime = 100;
+/* Assign actions */
+$('.product-quantity input').change( function() {
+  updateQuantity(this);
+});
+
+/* Recalculate cart */
+function recalculateCart()
+{
+  var subtotal = 0;
+  /* Sum up row totals */
+  $('.product').each(function () {
+    subtotal += parseFloat($(this).children('.product-line-price').text());
+  });
+  
+  
+  /* Update totals display */
+  $('.totals-value').fadeOut(fadeTime, function() {
+    $('#cart-subtotal').html(subtotal.toFixed(1));
+  });
+}
+/* Update quantity */
+function updateQuantity(quantityInput)
+{
+  /* Calculate line price */
+  var productRow = $(quantityInput).parent().parent();
+  var quantity = $(quantityInput).val();
+  var linePrice = 120 * quantity;
+  
+  /* Update line price display and recalc cart totals */
+  productRow.children('.product-line-price').each(function () {
+    $(this).fadeOut(fadeTime, function() {
+      $(this).text(linePrice.toFixed(1));
+      recalculateCart();
+      $(this).fadeIn(fadeTime);
+    });
+  });  
+}

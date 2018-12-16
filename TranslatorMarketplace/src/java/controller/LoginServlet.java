1a50489820ab4f39d4e39ebec625d2fdd9cf183f
thanat1389 <59070074@it.kmitl.ac.th>
2018-12-16 16:55:18 +0700

    สร้างหน้า LoginServlet เพื่อเช็คตอน login + แก้ name attribute + สร้าง form หน้า Login.html

---
+++ TranslatorMarketplace/src/java/controller/LoginServlet.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import javax.sql.DataSource;
+import model.file_create;
+
+/**
+ *
+ * @author may
+ */
+@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+
+    private Connection conn;
+
+    public void init() {
+        conn = (Connection) getServletContext().getAttribute("connection");
+    }
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            //แสดงผลลัพธ์เป็นภาษาไทยได้ถูกต้อง
+            request.setCharacterEncoding("UTF-8");
+
+            //รับค่า parameter จาก Login.html
+            String id = request.getParameter("id");
+            String password = request.getParameter("password");
+            
+
+            ServletContext session = request.getServletContext();
+
+            //เช็ค id ว่ามีใน customers table หรือไม่
+            String sql = "SELECT * FROM customers WHERE id_customer=?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, id);
+            ResultSet rs = pstmt.executeQuery();
+            
+            //ถ้ามีก็ไปเช็ค password ว่าตรงกับที่รีจิสไว้หรือไม่
+            if (id.equals("sql.id_customer") && password.equals("sql.password")) {
+                //ถ้าตรงก็ให้ไปหน้า Order_customer
+                response.sendRedirect("Order_customer.jsp");
+            } else {
+                //ถ้าไม่ตรงให้แจ้งเตือน *** ยังไม่แน่ใจหน้าที่ response อ้ะ ต้อง response ป้ะ***
+                out.println("alert('Invalid ID or Password, please try again!!')");
+                response.sendRedirect("Login.html");
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(RegisterServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
